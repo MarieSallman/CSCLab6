dc72a02580869594b17b1b7b67bc1c609bcc8331
marie <Marie.Sallman@trojans.dsu.edu>
2019-11-12 15:40:42 -0600

    Testing

---
+++ src/FibExamples.java
@@ -0,0 +1,61 @@
+import java.math.BigInteger;
+import java.math.BigDecimal;
+
+public class FibExamples {
+
+    public static void main(String[] args) {
+       int count = 8;
+
+        System.out.println(fibMatrixBig(count));
+        System.out.println(fibLoopBig(count));
+    }
+
+
+    private static BigInteger fibMatrixBig(int n) {
+        BigInteger[] matrix = {BigInteger.ONE, BigInteger.ONE, BigInteger.ONE, BigInteger.ZERO};
+        return matrixPow(matrix, n)[1];
+    }
+
+    // Computes the power of a matrix. The matrix is packed in row-major order.
+    private static BigInteger[] matrixPow(BigInteger[] matrix, int n) {
+        if (n < 0)
+            throw new IllegalArgumentException();
+        BigInteger[] result = {BigInteger.ONE, BigInteger.ZERO, BigInteger.ZERO, BigInteger.ONE};
+        while (n != 0) {  // Exponentiation by squaring
+            if (n % 2 != 0)
+                result = matrixMultiply(result, matrix);
+            n /= 2;
+            matrix = matrixMultiply(matrix, matrix);
+        }
+        return result;
+    }
+
+    // Multiplies two matrices.
+    private static BigInteger[] matrixMultiply(BigInteger[] x, BigInteger[] y) {
+        return new BigInteger[] {
+                multiply(x[0], y[0]).add(multiply(x[1], y[2])),
+                multiply(x[0], y[1]).add(multiply(x[1], y[3])),
+                multiply(x[2], y[0]).add(multiply(x[3], y[2])),
+                multiply(x[2], y[1]).add(multiply(x[3], y[3]))
+        };
+    }
+
+
+    private static BigInteger fibLoopBig(int n) {
+        BigInteger a = BigInteger.ZERO;
+        BigInteger b = BigInteger.ONE;
+        for (int i = 0; i < n; i++) {
+            BigInteger c = a.add(b);
+            a = b;
+            b = c;
+        }
+        return a;
+    }
+
+    // Multiplies two BigIntegers. This function makes it easy to swap in a faster algorithm like Karatsuba multiplication.
+    private static BigInteger multiply(BigInteger x, BigInteger y) {
+        return x.multiply(y);
+    }
+
+
+}
